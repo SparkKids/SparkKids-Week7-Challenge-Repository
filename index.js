d5d353f94a74008ed3ed098b2ba5931f4709cfbe
Steven Gray <stevegray@granite-mtn.com>
2024-12-04 02:19:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,137 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const readMeFile = "README.md";
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input
+
+const questions = ["Please enter your project title:",
+    "Please enter a description for your project:",
+    "Please enter the installation instructions for your application:",
+    "Please enter the command to run your code:",
+    "Please enter the guidelines for contributing to the project:",
+    "Please enter testing instructions:",
+    "Please select your license:",
+    "Please enter your GitHub user name:",
+    "Please enter your email address:"];
+
+const licenses = ["Apache 2.0 License", "BSD 2-Clause License", "BSD 3-Clause License",
+    "GNU GPL v2", "GNU GPL v3", "The MIT License", "Mozilla Public License 2.0"];
+
+// TODO: Create a function to write README file
+async function writeToFile(fileName, data) {
+    try {
+        await fs.writeFile(fileName, data, (err) => {
+            if (err) throw err;
+        });
+        console.log('Success!');
+    } catch (err) {
+        console.error(err);
+    }
+}
+// TODO: Create a function to initialize app
+
+async function init() {
+    try {
+        const answers = await inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: questions[0],
+                validate: (value) => {
+                    if (value.trim().length === 0) {
+                        return 'Please enter a project title.';
+                    }
+                    return true;
+                }
+            },
+            /* {
+                type: 'input',
+                message: questions[1],
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'tests',
+            },*/
+            {
+                type: 'rawlist',
+                name: 'license',
+                message: questions[6],
+                choices: [licenses[0], licenses[1], licenses[2], licenses[3], licenses[4],
+                licenses[5], licenses[6]],
+            },/*
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'username',
+            },
+            {
+                type: 'input',
+                message: questions[8],
+                name: 'email',
+            }, */
+        ]);
+        console.log("answers.license = " + answers.license);
+
+        const readmeContent = `
+# ${answers.title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${answers.description || 'No description provided.'}
+
+## Installation
+${answers.installation || 'No installation instructions provided.'}
+
+## Usage
+${answers.usage || 'No usage information provided.'}
+
+## License
+${answers.license || 'No license information provided.'}
+
+## Contributing
+${answers.contributing || 'No contribution guidelines provided.'}
+
+## Tests
+${answers.tests || 'No tests provided.'}
+
+## Questions
+Feel free to contact me at ${answers.email || 'no@email.com'} if you have any questions about the project. You can find the GitHub repository at ${answers.github || 'https://github.com/'}.`;
+console.log("readmeContent = " + readmeContent);
+        var markdown = generateMarkdown(answers);
+        console.log("markdown = " + markdown);
+        console.log("writeToFile");
+        await writeToFile(readMeFile, markdown);
+        console.log('README.md has been generated!');
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,111 @@
+//Array of licenses user chooses from.
+const licenses = ["Apache 2.0 License", "BSD 2-Clause License", "BSD 3-Clause License",
+  "GNU GPL v2", "GNU GPL v3", "The MIT License", "Mozilla Public License 2.0"];
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+//10/19/2024 SGray - Done
+function renderLicenseBadge(pageIndex) {
+  const badges = ["https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+    "https://img.shields.io/badge/License-BSD_2--Clause-orange.svg",
+    "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
+    "https://img.shields.io/badge/License-GPL_v2-blue.svg",
+    "https://img.shields.io/badge/License-GPLv3-blue.svg",
+    "https://img.shields.io/badge/License-MIT-yellow.svg",
+    "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg"];
+  if (pageIndex == undefined) {
+    //User did not select a license, return a blank string
+    return "";
+  } else {
+    return badges[pageIndex];
+  }
+}//function renderLicenseBadge(pageIndex)
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+//10/19/2024 SGray - Done
+function renderLicenseLink(pageIndex) {
+  const licenselinks = ["https://opensource.org/licenses/Apache-2.0",
+    "https://opensource.org/licenses/BSD-2-Clause",
+    "https://opensource.org/licenses/BSD-3-Clause",
+    "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+    "https://www.gnu.org/licenses/gpl-3.0",
+    "https://opensource.org/licenses/MIT",
+    "https://opensource.org/licenses/MPL-2.0"];
+  if (pageIndex == undefined) {
+    //User did not select a license, return a blank string
+    return "";
+  } else {
+    return licenselinks[pageIndex];
+  }
+}//function renderLicenseLink(license)
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+  //All arrays will have the same index position. Save it so we only have 
+  //to look it up once.
+  var arraysIndex;
+  if (answers.license == undefined) {
+    arraysIndex = undefined;
+  } else {
+    arraysIndex = licenses.indexOf(answers.license);
+  }
+  const licenseBadge = renderLicenseBadge(arraysIndex);
+  const licenseLink = renderLicenseLink(arraysIndex);
+  return `
+ 
+
+  # ${answers.title}
+  ## License Badge
+ [![${answers.license}](${licenseBadge})](${licenseLink})
+
+ ## Description
+
+  ${answers.description} 
+    
+    
+  ## Table of Contents 
+       
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+      
+  ## Installation
+      
+  ${answers.installation} 
+  
+  ## Usage
+      
+  ${answers.usage}
+      
+      
+  ## Contributing
+
+  ${answers.contributing}
+
+  ## Tests
+
+  ${answers.tests}
+      
+    
+  ## License
+
+  [${answers.license}](${licenseLink})
+
+ 
+  ## Questions
+
+  [GitHub ${answers.username}](https://github.com/${answers.username})
+
+  [${answers.email}](mailto:${answers.email})
+`;
+}
+module.exports = generateMarkdown;
+
